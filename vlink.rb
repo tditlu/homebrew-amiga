3e9b03a8fa5dab295248ffb279c4a1e661967b39
tditlu <todi@tulou.org>
2014-01-10 10:07:35 +0100

    vlink portable multi file format linker

---
+++ vlink.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class Vlink < Formula
+  homepage 'http://sun.hasenbraten.de/vlink/'
+  url 'http://todi.se/brew/vlink/0.14a/vlink.tar.gz'
+  version '0.14b'
+  sha1 '1357873bca4e0a84839f5c2dfa3f7f43a1ed5883'
+
+  def install
+    mkdir 'object'
+
+    inreplace 'Makefile' do |s|
+      s.change_make_var! 'CC', "#{ENV.cc}"
+      s.change_make_var! 'COPTS', "-fomit-frame-pointer -c #{ENV.cflags}"
+      s.change_make_var! 'LD', "#{ENV.cc}"
+      s.change_make_var! 'LDOPTS', "#{ENV.ldflags}"
+    end
+
+
+    system 'make'
+    bin.install 'vlink'
+
+  end
+
+end
+
