314665acf056cc52d9c5c6170bb446abc7e12dde
Daniel Gerdgren <daniel@schimpanz.com>
2014-02-09 23:43:23 +0100

    Updated to 4.1

--- shrinkler.rb
+++ shrinkler.rb
@@ -2,16 +2,11 @@ require 'formula'
 
 class Shrinkler < Formula
   homepage 'https://bitbucket.org/askeksa/shrinkler'
-  url 'https://bitbucket.org/askeksa/shrinkler/get/569e994736e2.zip'
-  version '4.0.569e994736e2'
-  sha1 'f8ee9d2bacce206d8c57c19957f554630aa9922e'
+  url 'https://bitbucket.org/askeksa/shrinkler/get/819d8d8102ca.zip'
+  version '4.1.819d8d8102ca'
+  sha1 '6596824b23c8f7191aa35121ab58060ba1c7bc7a'
 
   def install
-    inreplace 'MatchFinder.h' do |s|
-      s.gsub! /static const int END_OF_CHAIN = -2;/, ''
-      s.gsub! /END_OF_CHAIN/, '-2'
-    end
-
     system 'make PLATFORM=native'
     File.rename 'build/native/Shrinkler', 'build/native/shrinkler'
     bin.install 'build/native/shrinkler'
