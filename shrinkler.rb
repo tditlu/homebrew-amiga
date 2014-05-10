7b0c5eb4738aaee75988fada39fab65fb857c592
tditlu <todi@tulou.org>
2014-05-10 11:53:32 +0200

    Changed to precompiled binary

--- shrinkler.rb
+++ shrinkler.rb
@@ -1,15 +1,13 @@
 require 'formula'
 
 class Shrinkler < Formula
-  homepage 'https://bitbucket.org/askeksa/shrinkler'
-  url 'https://bitbucket.org/askeksa/shrinkler/get/819d8d8102ca.zip'
-  version '4.1.819d8d8102ca'
-  sha1 '6596824b23c8f7191aa35121ab58060ba1c7bc7a'
+  homepage 'https://bitbucket.org/askeksa/shrinkler/'
+  url 'https://bitbucket.org/askeksa/shrinkler/downloads/shrinkler41.zip'
+  version '4.1'
+  sha1 '13ec6aeaf77a8d87094503ad7a432325c85bd34c'
 
   def install
-    system 'make PLATFORM=native'
-    File.rename 'build/native/Shrinkler', 'build/native/shrinkler'
-    bin.install 'build/native/shrinkler'
+    File.rename 'MacOSX/Shrinkler', 'MacOSX/shrinkler'
+    bin.install 'MacOSX/Shrinkler'
   end
-
 end
