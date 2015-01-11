395a06e7114d46277bdd4b3708660629ddd0c6cf
tditlu <todi@tulou.org>
2015-01-11 16:41:26 +0100

    Updated to vbcc 0.9d

--- vbcc.rb
+++ vbcc.rb
@@ -2,38 +2,50 @@ require 'formula'
 
 class Vbcc < Formula
   homepage 'http://www.compilers.de/vbcc.html'
-  url 'http://todi.se/brew/vbcc/0.9b/vbcc.tar.gz'
-  version '0.9b'
-  sha1 'f62c34d14d378142d3c5d94517965b5ec41ca858'
+  url 'http://todi.se/brew/vbcc/0.9d/vbcc.tar.gz'
+  version '0.9d'
+  sha1 '325d043b6224c584a9c29abcfebc236f5b774607'
+
+  resource "vbcc_target_cf_atari" do
+    url "http://todi.se/brew/vbcc/2014-12-30/vbcc_target_cf-atari.lha", :using => :nounzip
+    version '2014-12-30'
+    sha1 '3444ba7c29476f45fc9f3793db03e1fb1d1cd546'
+  end
 
   resource "vbcc_target_m68k_amigaos" do
-    url "http://todi.se/brew/vbcc/2011-08-06/vbcc_target_m68k-amigaos.lha", :using => :nounzip
-    version '2011-08-06'
-    sha1 'f0a7ac5ffb670a2deb5213867fd75d516774c459'
+    url "http://todi.se/brew/vbcc/2014-12-30/vbcc_target_m68k-amigaos.lha", :using => :nounzip
+    version '2014-12-30'
+    sha1 'f5942c7c8b57d9d54b0ba5bea534453c92e03205'
   end
 
   resource "vbcc_target_m68k_atari" do
-    url "http://todi.se/brew/vbcc/2011-08-06/vbcc_target_m68k-atari.lha", :using => :nounzip
-    version '2011-08-06'
-    sha1 'ee76920b2639057343ef322b95937c5493ef8dc7'
+    url "http://todi.se/brew/vbcc/2014-12-30/vbcc_target_m68k-atari.lha", :using => :nounzip
+    version '2014-12-30'
+    sha1 '5613329750f456c753b465f8f8be032bdcd4aed1'
   end
 
-  resource "vbcc_target_cf_atari" do
-    url "http://todi.se/brew/vbcc/2011-08-06/vbcc_target_cf-atari.lha", :using => :nounzip
-    version '2011-08-06'
-    sha1 '3caa2823466940dab5ce57cbdd914c225dc19915'
+  resource "vbcc_target_ppc_amigaos" do
+    url "http://todi.se/brew/vbcc/2014-12-30/vbcc_target_ppc-amigaos.lha", :using => :nounzip
+    version '2014-12-30'
+    sha1 'b3c8406a92cab47b6a6c9ab1cb8ca579544e0c40'
   end
 
   resource "vbcc_target_ppc_morphos" do
-    url "http://todi.se/brew/vbcc/2011-08-06/vbcc_target_ppc-morphos.lha", :using => :nounzip
-    version '2011-08-06'
-    sha1 '21bc3366d993d565478fd9d6413fe426afa6c049'
+    url "http://todi.se/brew/vbcc/2014-12-30/vbcc_target_ppc-morphos.lha", :using => :nounzip
+    version '2014-12-30'
+    sha1 '3162b761e990c43817e7c12f2c70609122fd4264'
   end
 
   resource "vbcc_target_ppc_powerup" do
-    url "http://todi.se/brew/vbcc/2011-08-06/vbcc_target_ppc-powerup.lha", :using => :nounzip
-    version '2011-08-06'
-    sha1 '603588b35594cf63cbb0fd8616c530aad765c880'
+    url "http://todi.se/brew/vbcc/2014-12-30/vbcc_target_ppc-powerup.lha", :using => :nounzip
+    version '2014-12-30'
+    sha1 '83131805cd7bf3d42cd452f0ecee007fd1abe243'
+  end
+
+  resource "vbcc_target_ppc_warpos" do
+    url "http://todi.se/brew/vbcc/2014-12-30/vbcc_target_ppc-warpos.lha", :using => :nounzip
+    version '2014-12-30'
+    sha1 '958e1cd8b412a60fc23b02c3589f22d48891f88a'
   end
 
   depends_on 'lha' => :build
@@ -53,7 +65,7 @@ class Vbcc < Formula
       s.change_make_var! 'LDFLAGS', "-lm #{ENV.ldflags}"
     end
 
-    targets = ['alpha', 'arm', 'c16x', 'generic', 'hc12', 'i386', 'm68k', 'm68ks', 'ppc', 'z']
+    targets = ['alpha', 'c16x', 'generic', 'hc12', 'i386', 'm68k', 'm68ks', 'ppc', 'z']
 
     targets.each do |target|
       system "make TARGET=#{target}"
@@ -65,6 +77,11 @@ class Vbcc < Formula
 
     (prefix/'targets').mkpath
 
+    resource("vbcc_target_cf_atari").stage do
+      system 'lha x vbcc_target_cf-atari.lha'
+      (prefix/'targets/cf-atari').install Dir['vbcc_target_cf-atari/targets/cf-atari/*']
+    end
+
     resource("vbcc_target_m68k_amigaos").stage do
       system 'lha x vbcc_target_m68k-amigaos.lha'
       (prefix/'targets/m68k-amigaos').install Dir['vbcc_target_m68k-amigaos/targets/m68k-amigaos/*']
@@ -75,9 +92,9 @@ class Vbcc < Formula
       (prefix/'targets/m68k-atari').install Dir['vbcc_target_m68k-atari/targets/m68k-atari/*']
     end
 
-    resource("vbcc_target_cf_atari").stage do
-      system 'lha x vbcc_target_cf-atari.lha'
-      (prefix/'targets/cf-atari').install Dir['vbcc_target_cf-atari/targets/cf-atari/*']
+    resource("vbcc_target_ppc_amigaos").stage do
+      system 'lha x vbcc_target_ppc-amigaos.lha'
+      (prefix/'targets/ppc-amigaos').install Dir['vbcc_target_ppc-amigaos/targets/ppc-amigaos/*']
     end
 
     resource("vbcc_target_ppc_morphos").stage do
@@ -88,6 +105,11 @@ class Vbcc < Formula
     resource("vbcc_target_ppc_powerup").stage do
       system 'lha x vbcc_target_ppc-powerup.lha'
       (prefix/'targets/ppc-powerup').install Dir['vbcc_target_ppc-powerup/targets/ppc-powerup/*']
+    end
+
+    resource("vbcc_target_ppc_warpos").stage do
+      system 'lha x vbcc_target_ppc-warpos.lha'
+      (prefix/'targets/ppc-warpos').install Dir['vbcc_target_ppc-warpos/targets/ppc-warpos/*']
     end
 
   end
