6d8e6e0c0915ebc1021ac3521ebeb9b0461d399f
tditlu <todi@tulou.org>
2015-02-20 08:24:42 +0100

    Updated to v4.4

--- shrinkler.rb
+++ shrinkler.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Shrinkler < Formula
   homepage 'https://bitbucket.org/askeksa/shrinkler/'
-  url 'https://bitbucket.org/askeksa/shrinkler/downloads/shrinkler43.zip'
-  version '4.3'
-  sha1 'd1324eb5786200ebb8378a26cbc6c3bd61b5765d'
+  url 'https://bitbucket.org/askeksa/shrinkler/downloads/shrinkler44.zip'
+  version '4.4'
+  sha1 'ca3ac5e59e2feb5f18cea04f59b56225dd6b5389'
 
   def install
     File.rename 'MacOSX/Shrinkler', 'MacOSX/shrinkler'
