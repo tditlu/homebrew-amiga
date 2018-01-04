04368dd55e38314b7543168daf7a56f15d84f944
Daniel Gerdgren <todi@tulou.org>
2018-01-04 09:25:34 +0100

    Update with real 0.16b version

--- vlink.rb
+++ vlink.rb
@@ -4,7 +4,7 @@ class Vlink < Formula
   homepage 'http://sun.hasenbraten.de/vlink/'
   url 'http://todi.se/brew/vlink/0.16b/vlink.tar.gz'
   version '0.16b'
-  sha256 '760d355864ce6f42581530cef39f9cc8375f5a72eb59934e859040523cc87311'
+  sha256 '7e55b2c36acee216c81580a4dd4a89f34e900df26e495b94cb568563cebf9741'
 
   def install
     mkdir 'objects'
