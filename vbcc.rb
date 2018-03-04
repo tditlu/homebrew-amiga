1267a06e03e2b02db08fca26154b2ebbf7d0feda
Daniel Gerdgren <todi@tulou.org>
2018-03-04 15:50:26 +0100

    Add download mirrors

--- vbcc.rb
+++ vbcc.rb
@@ -1,23 +1,27 @@
 class Vbcc < Formula
   homepage 'http://www.compilers.de/vbcc.html'
-  url 'http://todi.se/brew/vbcc/0.91f/vbcc.tar.gz'
+  url 'http://server.owl.de/~frank/tags/vbcc0_9fP1.tar.gz'
+  mirror 'http://todi.se/brew/vbcc/0.91f/vbcc.tar.gz'
   version '0.911f'
   sha256 '63a652c07b001a138566a84dd6059403cac325c597b646acb41f70a9d0ea4097'
 
-  resource "vbcc_target_m68k_amigaos" do
-    url "http://todi.se/brew/vbcc/2018-02-07/vbcc_target_m68k-amigaos.lha", :using => :nounzip
-    version '2018-02-07'
+  resource 'vbcc_target_m68k_amigaos' do
+    url 'http://server.owl.de/~frank/vbcc/2017-08-14/vbcc_target_m68k-amigaos.lha', :using => :nounzip
+    mirror 'http://todi.se/brew/vbcc/2017-08-14/vbcc_target_m68k-amigaos.lha'
+    version '2017-08-14'
     sha256 '515e471b007b3cfe2ba2d83445d08c1d3696b48e93f106d070f7ee4eb72b0f42'
   end
 
   resource "vbcc_target_m68k_kick13" do
-    url "http://todi.se/brew/vbcc/2018-02-07/vbcc_target_m68k-kick13.lha", :using => :nounzip
-    version '2018-02-07'
+    url 'http://server.owl.de/~frank/vbcc/2017-08-14/vbcc_target_m68k-kick13.lha', :using => :nounzip
+    mirror 'http://todi.se/brew/vbcc/2017-08-14/vbcc_target_m68k-kick13.lha'
+    version '2017-08-14'
     sha256 '68b73adb66dbb59302c9cb06f0a7fb91171b0b967a974d2d220916ac65926573'
   end
 
   resource "vbcc_unix_config" do
-    url "http://todi.se/brew/vbcc/2017-08-14/vbcc_unix_config.tar.gz"
+    url 'http://server.owl.de/~frank/vbcc/2017-08-14/vbcc_unix_config.tar.gz'
+    mirror 'http://todi.se/brew/vbcc/2017-08-14/vbcc_unix_config.tar.gz'
     version '2017-08-14'
     sha256 '15e24da750943c50648fa0e8aca8a0d540c9a61610bfd62c31672715b0ec1d45'
   end
