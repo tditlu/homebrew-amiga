cd25b584c0b3bc91863e4683dc0f64feb678a760
Daniel Gerdgren <todi@tulou.org>
2018-05-14 21:34:36 +0200

    Add RNC Propack Formula
    https://github.com/lab313ru/rnc_propack_source/

---
+++ propack.rb
@@ -0,0 +1,12 @@
+class Propack < Formula
+  homepage 'https://github.com/lab313ru/rnc_propack_source/'
+  url 'http://todi.se/brew/rnc_propack_source/1.2/rnc_propack_source.tar.gz'
+  version '1.2'
+  sha256 '85a3df783009aecd4579354c2b2d0a3bf528135ea11e7186a186c1d4e95b46f5'
+
+  def install
+    system 'make'
+    File.rename 'rnc64', 'propack'
+    bin.install 'propack'
+  end
+end
