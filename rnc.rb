7b433ed941d55f1de78d156dbf2c664c4dfc421e
Daniel Gerdgren <todi@tulou.org>
2018-05-31 17:41:15 +0200

    Update rnc to v1.3

--- rnc.rb
+++ rnc.rb
@@ -1,8 +1,8 @@
 class Rnc < Formula
   homepage 'https://github.com/lab313ru/rnc_propack_source/'
-  url 'http://todi.se/brew/rnc_propack_source/1.2/rnc_propack_source.tar.gz'
-  version '1.2'
-  sha256 '85a3df783009aecd4579354c2b2d0a3bf528135ea11e7186a186c1d4e95b46f5'
+  url 'http://todi.se/brew/rnc_propack_source/1.3/rnc_propack_source.tar.gz'
+  version '1.3'
+  sha256 '3c46b6f9a47824cfea7963e631d0c774e6430a8cb08f586789f8926c34a6089d'
 
   def install
     system 'make'
