ee4ce9e20346b34ef883c9404f11e8fad2c9328c
Daniel Gerdgren <todi@tulou.org>
2018-11-15 14:23:14 +0100

    Update RNC to version 1.4

--- rnc.rb
+++ rnc.rb
@@ -1,8 +1,8 @@
 class Rnc < Formula
   homepage 'https://github.com/lab313ru/rnc_propack_source/'
-  url 'http://todi.se/brew/rnc_propack_source/1.3/rnc_propack_source.tar.gz'
-  version '1.3'
-  sha256 '3c46b6f9a47824cfea7963e631d0c774e6430a8cb08f586789f8926c34a6089d'
+  url 'http://todi.se/brew/rnc_propack_source/1.4/rnc_propack_source.tar.gz'
+  version '1.4'
+  sha256 '5524faa9ec4c228b7600a42473a647414997da026a9cdb512f621081da8f2497'
 
   def install
     system 'make'
