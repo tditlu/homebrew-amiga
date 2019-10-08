7452b6859f583b62a6ba56bbdf26b537b9078b6e
Daniel Gerdgren <todi@tulou.org>
2019-10-08 11:17:44 +0200

    Update sha256 hash

--- elf2hunk.rb
+++ elf2hunk.rb
@@ -1,7 +1,7 @@
 class Elf2hunk < Formula
   homepage "https://github.com/BartmanAbyss/vscode-amiga-debug/blob/master/bin/elf2hunk.c"
   url "https://todi.se/brew/elf2hunk/0.7.0/elf2hunk-0.7.0.zip"
-  sha256 "d4975ab699d85041440a906c4dc17f9c505c7bbbd67b7fcefb3d98089e7375c3"
+  sha256 "68c70000b03fc83ee3f16f63b2dab1f0aa6ca831408864a2467cb7f35bcfbdca"
 
   def install
     system 'make'
