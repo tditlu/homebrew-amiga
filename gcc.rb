ed1ceaa76b9b0e3a8fe3b2d53fdf2c0a1534465a
Daniel Gerdgren <todi@tulou.org>
2019-10-08 11:33:40 +0200

    Update build script

--- gcc.rb
+++ gcc.rb
@@ -1,6 +1,6 @@
 class Gcc < Formula
   homepage "https://github.com/BartmanAbyss/gcc/tree/amiga-8_3_0"
-  url "http://todi.se/brew/gcc_amiga/8.3.0/gcc-amiga-8_3_0.zip"
+  url "http://todi.se/brew/gcc_amiga/8.3.0/gcc-amiga-8.3.0.zip"
   sha256 "9b22c6683f4ca3045c0f3cbb7cf3a7ef137ecf64fa90896d2788b2bff9d37b60"
 
   depends_on "gmp"
@@ -8,14 +8,11 @@ class Gcc < Formula
   depends_on "libmpc"
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-
-                          "--target=m68k-amiga-elf",
+    system "./configure", "--target=m68k-amiga-elf",
                           "--disable-nls",
                           "--enable-languages=c",
                           "--enable-lto",
+                          "--prefix=#{prefix}",
                           "--disable-libssp",
                           "--disable-gcov",
                           "--disable-multilib",
@@ -27,7 +24,7 @@ class Gcc < Formula
                           "--disable-libvtv",
                           "--disable-nls",
                           "--disable-clocale",
-                          "--prefix=#{prefix}"
+                          "--host=x86_64-apple-darwin"
     system "make", "all-gcc", "-j6"
     system "make", "install-gcc"
   end
