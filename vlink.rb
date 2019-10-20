9249ea764a66b698f1485d460d78548d1211f5d8
Daniel Gerdgren <todi@tulou.org>
2019-10-20 22:41:36 +0200

    Update vlink hash

--- vlink.rb
+++ vlink.rb
@@ -1,9 +1,9 @@
 class Vlink < Formula
   homepage 'http://sun.hasenbraten.de/vlink/'
-  url 'http://server.owl.de/~frank/tags/vlink0_16c.tar.gz'
-  mirror 'http://todi.se/brew/vlink/0.16c/vlink.tar.gz'
+  url 'https://server.owl.de/~frank/tags/vlink0_16c.tar.gz'
+  mirror 'https://todi.se/brew/vlink/0.16c/vlink.tar.gz'
   version '0.16c'
-  sha256 '35ef34fee6b838b449a65f1447224c4e1dd96a310377ccc346a9ffa7464d167a'
+  sha256 '558267887bce7ac5b49a0dd3d8ac0a5251c0b1ae3ed8c9439ad5b876994c413d'
 
   def install
     mkdir 'objects'
@@ -15,10 +15,8 @@ class Vlink < Formula
       s.change_make_var! 'LDOPTS', "#{ENV.ldflags}"
     end
 
-
     system 'make'
     bin.install 'vlink'
 
   end
-
 end
