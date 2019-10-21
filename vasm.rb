0aa6bd804d158f59994fc51ae8ede3cc6df5a05d
Daniel Gerdgren <todi@tulou.org>
2019-10-21 09:29:31 +0200

    Update vasm hash

--- vasm.rb
+++ vasm.rb
@@ -1,15 +1,15 @@
 class Vasm < Formula
   homepage 'http://sun.hasenbraten.de/vasm/'
-  url 'http://todi.se/brew/vasm/1.8g/vasm.tar.gz'
+  url 'https://todi.se/brew/vasm/1.8g/vasm.tar.gz'
   version '1.8g'
-  sha256 '2af245c1dc36523b43db48e97082ac6644a905a67e9baebf47ac03defe270051'
+  sha256 'c5189133ad1ef93feaea3c610d454b114bb1b8775c6c5e3cf1d3b32194d09743'
 
   def install
     system "mkdir -p obj"
 
     inreplace 'Makefile' do |s|
-      s.change_make_var! 'COPTS', "-c -O2 -DOUTAOUT -DOUTBIN -DOUTELF -DOUTHUNK -DOUTSREC -DOUTTOS -DOUTVOBJ #{ENV.cflags}"
-      s.change_make_var! 'LDFLAGS', "-lm #{ENV.ldflags}"
+        s.change_make_var! 'COPTS', "-c -O2 -DOUTAOUT -DOUTBIN -DOUTELF -DOUTHUNK -DOUTSREC -DOUTTOS -DOUTVOBJ #{ENV.cflags}"
+        s.change_make_var! 'LDFLAGS', "-lm #{ENV.ldflags}"
     end
 
     system "make CPU=m68k SYNTAX=mot vasmm68k_mot"
@@ -18,5 +18,5 @@ class Vasm < Formula
     system 'make vobjdump'
     bin.install 'vobjdump'
 
-  end
+    end
 end
