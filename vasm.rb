849ac670e4fa7a5173625dd83c03f28ea92d9ec1
Daniel Gerdgren <todi@tulou.org>
2019-11-28 13:40:38 +0100

    Update vasm file hash

--- vasm.rb
+++ vasm.rb
@@ -1,16 +1,16 @@
 class Vasm < Formula
-    homepage 'http://sun.hasenbraten.de/vasm/'
-    url 'http://sun.hasenbraten.de/vasm/release/vasm.tar.gz'
-    mirror 'https://todi.se/brew/vasm/1.8g/vasm.tar.gz'
-    version '1.8g'
-    sha256 'c5189133ad1ef93feaea3c610d454b114bb1b8775c6c5e3cf1d3b32194d09743'
+  homepage 'http://sun.hasenbraten.de/vasm/'
+  url 'https://todi.se/brew/vasm/1.8g/vasm.tar.gz'
+  mirror 'https://server.owl.de/~frank/tags/vasm1_8g.tar.gz'
+  version '1.8g'
+  sha256 'a554f1a8ca2310a1f8b54ab7db2067910ab7e14e0bf56f27dd2d2873a5f70599'
 
-    def install
+  def install
     system "mkdir -p obj"
 
     inreplace 'Makefile' do |s|
-        s.change_make_var! 'COPTS', "-c -O2 -DOUTAOUT -DOUTBIN -DOUTELF -DOUTHUNK -DOUTSREC -DOUTTOS -DOUTVOBJ #{ENV.cflags}"
-        s.change_make_var! 'LDFLAGS', "-lm #{ENV.ldflags}"
+      s.change_make_var! 'COPTS', "-c -O2 -DOUTAOUT -DOUTBIN -DOUTELF -DOUTHUNK -DOUTSREC -DOUTTOS -DOUTVOBJ #{ENV.cflags}"
+      s.change_make_var! 'LDFLAGS', "-lm #{ENV.ldflags}"
     end
 
     system "make CPU=m68k SYNTAX=mot vasmm68k_mot"
@@ -19,5 +19,5 @@ class Vasm < Formula
     system 'make vobjdump'
     bin.install 'vobjdump'
 
-    end
+  end
 end
