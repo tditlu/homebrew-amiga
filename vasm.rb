7b990ee76dfad5756d7e3bf186c27ba3f09ac043
Daniel Gerdgren <todi@tulou.org>
2019-11-28 12:55:00 +0100

    Add mirror

--- vasm.rb
+++ vasm.rb
@@ -1,10 +1,11 @@
 class Vasm < Formula
-  homepage 'http://sun.hasenbraten.de/vasm/'
-  url 'https://todi.se/brew/vasm/1.8g/vasm.tar.gz'
-  version '1.8g'
-  sha256 'c5189133ad1ef93feaea3c610d454b114bb1b8775c6c5e3cf1d3b32194d09743'
+    homepage 'http://sun.hasenbraten.de/vasm/'
+    url 'http://sun.hasenbraten.de/vasm/release/vasm.tar.gz'
+    mirror 'https://todi.se/brew/vasm/1.8g/vasm.tar.gz'
+    version '1.8g'
+    sha256 'c5189133ad1ef93feaea3c610d454b114bb1b8775c6c5e3cf1d3b32194d09743'
 
-  def install
+    def install
     system "mkdir -p obj"
 
     inreplace 'Makefile' do |s|
