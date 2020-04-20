e5e04e2ff2f5bdb7bba26d0a8a2c5be04650ed50
Daniel Gerdgren <todi@tulou.org>
2020-04-20 12:12:36 +0200

    Update vlink version

--- vlink.rb
+++ vlink.rb
@@ -1,9 +1,9 @@
 class Vlink < Formula
-  homepage 'http://sun.hasenbraten.de/vlink/'
-  url 'https://server.owl.de/~frank/tags/vlink0_16c.tar.gz'
-  mirror 'https://todi.se/brew/vlink/0.16c/vlink.tar.gz'
-  version '0.16c'
-  sha256 '35ef34fee6b838b449a65f1447224c4e1dd96a310377ccc346a9ffa7464d167a'
+    homepage 'http://sun.hasenbraten.de/vlink/'
+    url 'http://sun.hasenbraten.de/vlink/release/vlink.tar.gz'
+    mirror 'https://todi.se/brew/vlink/0.16d/vlink.tar.gz'
+    version '0.16d'
+    sha256 '40ac51ab5fe27934800cb60687932d9f29a54e9f317399c643b3538ae8ff005f'
 
   def install
     mkdir 'objects'
