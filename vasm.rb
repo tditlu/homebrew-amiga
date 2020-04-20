9efa9775eb36deba58b66ca017b661843373f196
Daniel Gerdgren <todi@tulou.org>
2020-04-20 12:13:04 +0200

    Update vasm.rb
    Update vasm version

--- vasm.rb
+++ vasm.rb
@@ -1,9 +1,9 @@
 class Vasm < Formula
   homepage 'http://sun.hasenbraten.de/vasm/'
-  url 'https://todi.se/brew/vasm/1.8g/vasm.tar.gz'
-  mirror 'https://server.owl.de/~frank/tags/vasm1_8g.tar.gz'
-  version '1.8g'
-  sha256 'a554f1a8ca2310a1f8b54ab7db2067910ab7e14e0bf56f27dd2d2873a5f70599'
+  url 'https://todi.se/brew/vasm/1.8h/vasm.tar.gz'
+
+  version '1.8h'
+  sha256 '2b7aba9b6d0a196a2ab009fbed08f10acd94da41d11d3a224cb59b2a6c2f2b41'
 
   def install
     system "mkdir -p obj"
