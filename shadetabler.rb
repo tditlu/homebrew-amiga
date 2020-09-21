80cbc7b8359d2294614809bcaf07691b516630dd
Daniel Gerdgren <todi@tulou.org>
2020-09-21 22:35:44 +0200

    Update ShadeTabler to v1.0.2

--- shadetabler.rb
+++ shadetabler.rb
@@ -1,9 +1,9 @@
 class Shadetabler < Formula
   desc ""
   homepage "https://github.com/tditlu/shadetabler"
-  url "http://todi.se/brew/shadetabler/1.0.1/shadetabler.zip"
-  version "1.0.1"
-  sha256 "ccb521ba60b41af6784000462900e5b026762c2f38edb5ea4b6ac1f5666e061b"
+  url "http://todi.se/brew/shadetabler/1.0.2/shadetabler.zip"
+  version "1.0.2"
+  sha256 "871a5799b014c07afbaac057fb07702e1aa9c44ea0656fe0c4383d4ab2091af2"
 
   def install
     system 'make'
