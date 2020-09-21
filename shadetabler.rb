3fbe25e13ee0a8990a6ba79484f251de52520008
Daniel Gerdgren <todi@tulou.org>
2020-09-21 21:39:18 +0200

    Add ShadeTabler v1.0.0

---
+++ shadetabler.rb
@@ -0,0 +1,13 @@
+class Amigeconv < Formula
+  desc ""
+  homepage "https://github.com/tditlu/shadetabler"
+  url "http://todi.se/brew/shadetabler/1.0.0/shadetabler.zip"
+  version "1.0.0"
+  sha256 "687784ff236d912cc1465ee17cf9160095c39351f02c7d7e62970d7cfd3787fc"
+
+  def install
+    system 'make'
+    bin.install Dir['bin/*']
+  end
+end
+
