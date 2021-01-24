c27a22a62064e94e7080bd9db55f61f219a9fc6b
Daniel Gerdgren <daniel@oldapes.com>
2021-01-24 20:06:20 +0100

    Update vasm & vlink mirror

--- vasm.rb
+++ vasm.rb
@@ -1,23 +1,23 @@
 class Vasm < Formula
     homepage 'http://sun.hasenbraten.de/vasm/'
     url 'https://todi.se/brew/vasm/1.8j/vasm.tar.gz'
-
+    mirror 'https://server.owl.de/~frank/tags/vasm1_8j.tar.gz'
     version '1.8j'
     sha256 '8b8b78091d82a92769778b2964e64c4fb98e969b46d65708dcf88f6957072676'
-
+  
     def install
-        system "mkdir -p obj"
-
-        inreplace 'Makefile' do |s|
-            s.change_make_var! 'COPTS', "-c -O2 -DOUTAOUT -DOUTBIN -DOUTELF -DOUTHUNK -DOUTSREC -DOUTTOS -DOUTVOBJ #{ENV.cflags}"
-            s.change_make_var! 'LDFLAGS', "-lm #{ENV.ldflags}"
-        end
-
-        system "make CPU=m68k SYNTAX=mot vasmm68k_mot"
-        bin.install "vasmm68k_mot"
-
-        system 'make vobjdump'
-        bin.install 'vobjdump'
-
+      system "mkdir -p obj"
+  
+      inreplace 'Makefile' do |s|
+        s.change_make_var! 'COPTS', "-c -O2 -DOUTAOUT -DOUTBIN -DOUTELF -DOUTHUNK -DOUTSREC -DOUTTOS -DOUTVOBJ #{ENV.cflags}"
+        s.change_make_var! 'LDFLAGS', "-lm #{ENV.ldflags}"
+      end
+  
+      system "make CPU=m68k SYNTAX=mot vasmm68k_mot"
+      bin.install "vasmm68k_mot"
+  
+      system 'make vobjdump'
+      bin.install 'vobjdump'
+  
     end
-end
+  end
--- vlink.rb
+++ vlink.rb
@@ -1,21 +1,22 @@
 class Vlink < Formula
     homepage 'http://sun.hasenbraten.de/vlink/'
-    url 'https://todi.se/brew/vlink/0.16g/vlink.tar.gz'
-
+    url 'https://server.owl.de/~frank/tags/vlink0_16g.tar.gz'
+    mirror 'https://todi.se/brew/vlink/0.16g/vlink.tar.gz'
     version '0.16g'
     sha256 '4792740bda3850e82d22bd87d54a918c81571eff0fc447c52b94b2af38e90ba4'
 
-    def install
-        mkdir 'objects'
+  def install
+    mkdir 'objects'
 
-        inreplace 'Makefile' do |s|
-            s.change_make_var! 'CC', "#{ENV.cc}"
-            s.change_make_var! 'COPTS', "-fomit-frame-pointer -c #{ENV.cflags}"
-            s.change_make_var! 'LD', "#{ENV.cc}"
-            s.change_make_var! 'LDOPTS', "#{ENV.ldflags}"
-        end
-
-        system 'make'
-        bin.install 'vlink'
+    inreplace 'Makefile' do |s|
+      s.change_make_var! 'CC', "#{ENV.cc}"
+      s.change_make_var! 'COPTS', "-fomit-frame-pointer -c #{ENV.cflags}"
+      s.change_make_var! 'LD', "#{ENV.cc}"
+      s.change_make_var! 'LDOPTS', "#{ENV.ldflags}"
     end
+
+    system 'make'
+    bin.install 'vlink'
+
+  end
 end
