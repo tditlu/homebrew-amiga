49faf6d742cc0e7d8703c31641b7d231ce84aa49
Daniel Gerdgren <daniel@oldapes.com>
2021-02-18 16:01:51 +0100

    Update Amigeconv to version 1.0.3

--- amigeconv.rb
+++ amigeconv.rb
@@ -1,9 +1,9 @@
 class Amigeconv < Formula
   desc ""
   homepage "https://github.com/tditlu/amigeconv"
-  url "http://todi.se/brew/amigeconv/1.0.2/amigeconv.zip"
-  version "1.0.2"
-  sha256 "4a625b0701c10487e645d353315ea0199660901fbeecc321a52827a4d58c49f8"
+  url "http://todi.se/brew/amigeconv/1.0.3/amigeconv.zip"
+  version "1.0.3"
+  sha256 "12004398bb80f1f4350c150198b93fbb982d9b53892c9290c7ce5332c8e23745"
 
   def install
     system 'make'
