c3de444db19ab3c05f2a327dcf3ce3d047191797
Daniel Gerdgren <daniel@oldapes.com>
2022-02-28 07:50:39 +0100

    Version update Shrinkler (v4.7)

--- shrinkler.rb
+++ shrinkler.rb
@@ -1,10 +1,10 @@
 class Shrinkler < Formula
   desc "Shrinkler executable file compressor for Amiga by Blueberry"
   homepage "https://github.com/askeksa/Shrinkler"
-  url "http://todi.se/brew/shrinkler/4.6/shrinkler.zip"
-  mirror 'http://crinkler.net/shrinkler46.zip'
-  version "4.6"
-  sha256 "2046ac4a5336135741b8e03f0dab3c4070a13eeef52343bcb5573de071956355"
+  url "http://todi.se/brew/shrinkler/4.7/shrinkler.zip"
+  mirror 'http://crinkler.net/shrinkler47.zip'
+  version "4.7"
+  sha256 "4a56ce76271065101bdb73d6f87c13f75b39eb3e4cffab600a37e809829de834"
 
   def install
     File.rename 'MacOSX/Shrinkler', 'MacOSX/shrinkler'
