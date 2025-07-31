be2a7f240f0916f0df4c2a79c1bfccb16bbf1ffb
Daniel Gerdgren <todi@tulou.org>
2025-07-31 14:22:40 +0200

    Upgrade vasm to v2.0c

--- vasm.rb
+++ vasm.rb
@@ -1,9 +1,9 @@
 class Vasm < Formula
   homepage 'http://sun.hasenbraten.de/vasm/'
-  url 'https://todi.se/brew/vasm/2.0a/vasm.tar.gz'
-  mirror 'http://phoenix.owl.de/tags/vasm2_0a.tar.gz'
-  version '2.0a'
-  sha256 '413181dae18f7d566c4aeee669039ea20083817c412a402eed5ec8490f7b9d76'
+  url 'https://todi.se/brew/vasm/2.0c/vasm.tar.gz'
+  mirror 'http://phoenix.owl.de/tags/vasm2_0c.tar.gz'
+  version '2.0c'
+  sha256 'ca9c5b260762790c23017184e406fbf242aed8c3e7a722b12266cd7bff70af9e'
 
   def install
     system "mkdir -p obj"
