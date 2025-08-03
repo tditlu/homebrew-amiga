9599e31deb2d9735fbb365466de630682897231d
Daniel Gerdgren <todi@tulou.org>
2025-08-03 09:29:58 +0200

    Update Amigaconv to v1.1.1

--- amigeconv.rb
+++ amigeconv.rb
@@ -1,9 +1,9 @@
 class Amigeconv < Formula
   desc ""
   homepage "https://github.com/tditlu/amigeconv"
-  url "http://todi.se/brew/amigeconv/1.1.0/amigeconv.zip"
-  version "1.1.0"
-  sha256 "fea5449407407f72aa3f46d0fd5cc27490c78057cf5771243f8f8e63842c3f26"
+  url "http://todi.se/brew/amigeconv/1.1.1/amigeconv.zip"
+  version "1.1.1"
+  sha256 "9dfa64b17696786616b5961a6ab627e02800498f8423c33475979b0514c601b3"
 
   def install
     system 'make'
