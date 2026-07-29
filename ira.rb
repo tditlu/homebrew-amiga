f89f93c7333dc29c4d1247d42bc1d3c6a3497e85
Daniel Gerdgren <todi@tulou.org>
2026-07-29 21:53:10 +0200

    Update IRA 2.11 archive

--- ira.rb
+++ ira.rb
@@ -1,10 +1,8 @@
 class Ira < Formula
   homepage 'http://sun.hasenbraten.de/~frank/projects/'
-  url 'https://todi.se/brew/ira/2.11/ira.lha'
+  url 'https://todi.se/brew/ira/2.11/ira.tar.gz'
   version '2.11'
-  sha256 'ee288906c5b5dc55a2b2dc677b4fed1530c95786073fdeb5423a9014406c5223'
-
-  depends_on "lhasa" => :build
+  sha256 'e9a42a941ebf1b1fb378bad1fcb882113636b61747ce1280b8c42797'
 
   def install
 #    Dir.chdir 'ira' do
